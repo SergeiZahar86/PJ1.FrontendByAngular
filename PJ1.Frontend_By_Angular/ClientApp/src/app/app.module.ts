2e4e87da967777720c10e0238a1e4faef8387bda
Сергей Захаров <sergei86zahar@mail.ru>
2022-04-22 17:57:28 +0200

    Добавил компоненты пустышки для тестирования

--- PJ1.Frontend_By_Angular/ClientApp/src/app/app.module.ts
+++ PJ1.Frontend_By_Angular/ClientApp/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ProtectedComponent } from './protected/protected.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnauthorizedComponent,
+    DashboardComponent,
+    LoginComponent,
+    LogoutComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
