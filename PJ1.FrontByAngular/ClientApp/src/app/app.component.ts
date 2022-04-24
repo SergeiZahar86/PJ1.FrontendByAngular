0b9903ebfbeaa1c27774482b4be44bfa4008a18c
Сергей Захаров <sergei86zahar@mail.ru>
2022-04-24 11:14:03 +0200

    Форматирование

--- PJ1.FrontByAngular/ClientApp/src/app/app.component.ts
+++ PJ1.FrontByAngular/ClientApp/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit{
   title = 'app';
+  
+  constructor(private authService: AuthService) {
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    
+  }
+  
+  
 }
--- PJ1.FrontByAngular/ClientApp/src/app/services/auth-guard.service.ts
+++ PJ1.FrontByAngular/ClientApp/src/app/services/auth-guard.service.ts
@@ -1,29 +1,28 @@
 import {Injectable} from '@angular/core';
 import {
-  ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree
+    ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree
 } from "@angular/router";
 import {Observable} from "rxjs";
 import {AuthService} from "./auth.service";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
+    constructor(private authService: AuthService, private router: Router) {
+    }
 
-  canActivate():
-      Observable<boolean | UrlTree> |
-      Promise<boolean | UrlTree> |
-      boolean | UrlTree
-  {
-    let isLoggedIn = this.authService.isLoggedInObs();
-    isLoggedIn.subscribe((loggedin) => {
-      if (!loggedin) {
-        this.router.navigate(['unauthorized']);
-      }
-    });
-    return isLoggedIn;
-  }
+    canActivate():
+        Observable<boolean | UrlTree> |
+        Promise<boolean | UrlTree> |
+        boolean | UrlTree {
+        let isLoggedIn = this.authService.isLoggedInObs();
+        isLoggedIn.subscribe((loggedin) => {
+            if (!loggedin) {
+                this.router.navigate(['unauthorized']);
+            }
+        });
+        return isLoggedIn;
+    }
 }
