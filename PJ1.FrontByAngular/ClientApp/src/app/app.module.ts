e9f0a6c729aedcf39a37b44532d247ecc67e6827
Сергей Захаров <sergei86zahar@mail.ru>
2022-04-24 10:59:20 +0200

    исправил ошибку связанную со стилями

--- PJ1.FrontByAngular/ClientApp/src/app/app.module.ts
+++ PJ1.FrontByAngular/ClientApp/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { WwwwComponent } from './wwww/wwww.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +16,7 @@ import { WwwwComponent } from './wwww/wwww.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
-    WwwwComponent
+    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
